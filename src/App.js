569eee8537ba1c5593b1a1195c3126d8a9163b71
waterpolymer <kendrick.shao28@myhunter.cuny.edu>
2020-01-10 13:34:26 -0500

    Added selectable color

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-import AddRow from './components/row'
+//import AddRow from './components/row'
 
 
 class App extends Component{
@@ -9,34 +9,40 @@ class App extends Component{
     this.state = {
       rowCount: 0,
       columnCount: 0,
-      color: "",
+      color: "white",
       isMouseDown: false
     }
   }
 
+<<<<<<< HEAD
   updateColor = (newColor) => {
     this.setState = ({
       color: newColor
+=======
+  updateColor = (event) => {
+    this.setState({
+      color: event.target.value
+>>>>>>> Added selectable color
     })
   }
 
   incrementRow = () => {
     this.setState({
       rowCount: this.state.rowCount + 1
-    })
+    });
   }
 
   incrementColumn = () => {
     this.setState({
       columnCount: this.state.columnCount + 1
-    })
+    });
   }
 
   decrementRow = () => {
     if(this.state.rowCount !== 0){
       this.setState({
         rowCount: this.state.rowCount - 1
-      })
+      });
     }
   }
 
@@ -44,7 +50,7 @@ class App extends Component{
     if(this.state.columnCount !== 0){
       this.setState({
         columnCount: this.state.columnCount - 1
-      })
+      });
     }
   }
 
@@ -59,16 +65,33 @@ class App extends Component{
         <header className="App-header">
           {/*
             Testable button
+             <AddRow rowCount={this.state.rowCount} columnCount={this.state.columnCount} color={this.state.color}></AddRow>
           */}
 
-          <AddRow rowCount={this.state.rowCount} columnCount={this.state.columnCount} color={this.state.color}></AddRow>
-
+         
+          <div>
           <button onClick={this.incrementRow}> Add row </button>
+          <button onClick={this.incrementColumn}> Add column </button>
+          <select onChange={this.updateColor}>
+            <option value="white"> White </option>
+            <option value="black"> Black </option>
+            <option value="blue"> Blue </option>
+            <option value="red"> Red </option>
+            <option value="yellow"> Yellow </option>
+          </select>
+          </div>
+
+          <div>
           row count: {this.state.rowCount}
+          </div>
 
+          <div>
           column count: {this.state.columnCount}
+          </div>
 
+          <div>
           color: { this.state.color}
+          </div>
 
         </header>
       </div>
